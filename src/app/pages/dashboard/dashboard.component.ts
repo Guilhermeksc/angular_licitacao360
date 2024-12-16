592413e28a0fac76f61c864db4645506bae28da1
GuilhermeK <guilhermekscampos@gmail.com>
2024-12-16 00:06:31 -0300

    add

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MinisidebarComponent } from './minisidebar/minisidebar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MinisidebarComponent, SidebarComponent, ContentComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
--- src/app/pages/dashboard/sidebar/sidebar.component.ts
+++ src/app/pages/dashboard/sidebar/sidebar.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-sidebar',
-  imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrls: ['./sidebar.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class SidebarComponent {
+  // Controle do estado dos menus
+  showSubMenuNavigate = false;
+  showSubMenu = false;
+  showSubMenuDocs = false;
 
+  toggleMenu(menu: string): void {
+    console.log(`Toggled menu: ${menu}`); // Verifica se o método é chamado
+    switch (menu) {
+      case 'navigate':
+        this.showSubMenuNavigate = !this.showSubMenuNavigate;
+        break;
+      case 'modules':
+        this.showSubMenu = !this.showSubMenu;
+        break;
+      case 'docs':
+        this.showSubMenuDocs = !this.showSubMenuDocs;
+        break;
+    }
+  }  
 }
